4e01be65960127ddee6edb58ca0cbcee9351793c
ktyudha <ktyudha@gmail.com>
2025-08-13 18:37:19 +0700

    feat: change resource package addon

--- app/Http/Resources/Package/PackageAddOnResource.php
+++ app/Http/Resources/Package/PackageAddOnResource.php
@@ -14,6 +14,10 @@ class PackageAddOnResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'price' => (int) $this->price,
+        ];
     }
 }
